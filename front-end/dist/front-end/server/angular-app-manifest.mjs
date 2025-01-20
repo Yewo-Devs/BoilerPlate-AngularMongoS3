228c65a55395f4c4aaa10c2263a38df2f666485c
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2025-01-20 16:40:30 +0200

    Added docker image after every build

--- front-end/dist/front-end/server/angular-app-manifest.mjs
+++ front-end/dist/front-end/server/angular-app-manifest.mjs
@@ -6,8 +6,8 @@ export default {
   locale: undefined,
   routes: undefined,
   assets: {
-    'index.csr.html': {size: 5350, hash: '59fe0ed6ce1b366aa6486b8de3d168a613e50522a9e40ac620699e26d60cd1e5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2375, hash: '6915fd3a469f17f021775ac7fce5155dcb1140bf69114c1fc3a3d65c39154c13', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5322, hash: '6ba0ce2fff98048e8bb694d65d664359831f71acf16978d3adf09892a5b608c9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2349, hash: 'f76f4d34e3f659b35864a6c0ea0c0a591d5a5042b54de2c310fe7d70842affde', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-WZ2IOCQ3.css': {size: 60322, hash: 'MgWNqBOL5Tc', text: () => import('./assets-chunks/styles-WZ2IOCQ3_css.mjs').then(m => m.default)}
   },
 };
